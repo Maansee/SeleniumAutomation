fbf2b0822347c6e955349ae742f1ef65499810cd
Maansee <maansee313@gmail.com>
2023-04-24 15:22:07 +0530

    added ecom code

--- SeleniumTraining/src/test/java/intro/AddPdtsToCart.java
+++ SeleniumTraining/src/test/java/intro/AddPdtsToCart.java
@@ -11,31 +11,64 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class AddPdtsToCart {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ChromeOptions ops = new ChromeOptions();
 		ops.addArguments("--remote-allow-origins=*");
 		WebDriver driver = new ChromeDriver(ops);
-		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		;
 		
 		int count=0;
-		String[] cartList= {"Cucumber","Tomato","Beetroot","Apple"};
+		String[] addItem = {"Cucumber","Tomato","Beetroot","Apple"};
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		Thread.sleep(3000);
 		
+		
+		List<String> addItemList = Arrays.asList(addItem);
+
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-		
-		for(int i=0;i<products.size();i++)
-		{
-			String[] item = products.get(i).getText().split("-");
-			String final_item=item[0].trim();
-			
+
+		List<WebElement> buttons = driver.findElements(By.xpath("//div[@class='product-action']/button"));
+
+		String[] productArray;
+
+		String product;
+
+		int j = 0;
+
+
+
+		for(int i = 1;i<products.size();i++) {
+
+		productArray = products.get(i).getText().split("-");
+
+		product = productArray[0].trim();
+
+		if (addItemList.contains(product)) {
+
+		j++;
+
+		buttons.get(i).click();
+
+		Thread.sleep(3000);
+
+		if (j == (addItem.length)) {
+
+		break;
+
 		}
-		
-		List finalCartList = Arrays.asList(cartList);
-		
-		if(finalCartList.contains(cartList))
-		{
-			count++;
-			driver.findElement(By.xpath("//div[@class='product-action']//button")).get(i).click();
+
 		}
+
+		}
+
+		//click on Cart bag
+
+		driver.findElement(By.xpath("//*[@id='root']/div/header/div/div[3]/a[4]/img")).click();
+
+		//click on Proceed to checkout
+
+		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		
 
 	}
 
