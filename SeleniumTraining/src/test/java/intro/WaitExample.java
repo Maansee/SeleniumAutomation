8fc305ab84d5b22a10cbe2dc99b4996799b0101f
Maansee <maansee313@gmail.com>
2023-05-02 13:41:03 +0530

    explixit wait code added

--- SeleniumTraining/src/test/java/intro/WaitExample.java
+++ SeleniumTraining/src/test/java/intro/WaitExample.java
@@ -1,5 +1,6 @@
 package intro;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -9,6 +10,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitExample {
 
@@ -17,7 +20,11 @@ public class WaitExample {
 		ChromeOptions ops = new ChromeOptions();
 		ops.addArguments("--remote-allow-origins=*");
 		WebDriver driver = new ChromeDriver(ops);
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		//implicit wait global declaration
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
 		
 		String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot"};
 		
@@ -28,9 +35,20 @@ public class WaitExample {
 		addItems(driver,itemsNeeded);
 		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
 		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+		
+		//explicit wait
+		
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+		
 		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.cssSelector("button.promoBtn")).click();
-		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());;
+		
+		//explicit wait
+		
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+		
+		
 	
 	}
 
