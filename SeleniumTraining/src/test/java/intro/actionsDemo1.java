d25d7bd40f2400d2a635075843eb6e831b0b0528
Maansee <maansee313@gmail.com>
2023-05-12 11:25:46 +0530

    added move hover code using actions

---
+++ SeleniumTraining/src/test/java/intro/actionsDemo1.java
@@ -0,0 +1,19 @@
+package intro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class actionsDemo1 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.in/");
+		Actions a = new Actions(driver);
+		a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-accountList']"))).build().perform();
+		
+	}
+
+}
